75d604973edaa4ea0051676e271e09e769de736d
liuaaahui <50660887+liuaaahui@users.noreply.github.com>
2020-06-17 21:48:47 +0800

    Update StudentTest.java

--- JavaOOP/src/StudentTest.java
+++ JavaOOP/src/StudentTest.java
@@ -2,7 +2,7 @@ import model.Student;
 
 public class StudentTest {
     public static void main(String[] args) {
-        Student student = new Student("penny",22,true,04160704);
+        Student student = new Student("penny",22,true,123);
         System.out.println("学生姓名：" + student.getName());
         System.out.println("学生年龄：" + student.getAge());
         System.out.println("学生性别：" + student.isGender());
